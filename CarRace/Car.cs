80e3bb68f1d875869623a3c58a36e07330382b98
Wettergrund <Jonas.wettergrund@gmail.com>
2023-04-04 10:04:06 +0200

    Add project files.

---
+++ CarRace/Car.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRace
+{
+    internal class Car
+    {
+        int warp = 10; // Spee factor 
+
+        public string CarName { get; set; }
+        private int MaxSpeed { get; set; } = 120;
+
+        public decimal CurrentSpeed { get; set; }
+
+        public ConsoleColor Color { get; set; }
+
+
+        public decimal TraveledDistance { get; set; } = 0;
+
+        public Car(string name, ConsoleColor color = ConsoleColor.White){
+            CarName = name;
+            
+            Color = color;
+        }
+
+        //Events
+        public async Task RunOutOfFuel()
+        {
+            decimal temp = CurrentSpeed;    
+
+            Console.WriteLine($"{CarName} Out of fuel +30sek");
+            CurrentSpeed = 0;
+            await Task.Delay(30000 / warp);
+
+            CurrentSpeed = temp;
+           
+        }
+
+        public async Task FlatTire()
+        {
+            decimal temp = CurrentSpeed;    
+
+            CurrentSpeed = 0;
+
+            Console.WriteLine($"{CarName} Flat tire +20sek");
+            await Task.Delay(20000 / warp);
+            CurrentSpeed = temp;
+
+
+        }
+
+        public async Task BirdStrike()
+        {
+            decimal temp = CurrentSpeed;
+
+            CurrentSpeed = 0;
+
+            Console.WriteLine($"Bird strike! {CarName} + 10sek");
+                await Task.Delay(10000 / warp);
+            CurrentSpeed = temp;
+
+
+        }
+
+        public async Task RoughEngine()
+        {
+
+
+                MaxSpeed--;
+                Console.WriteLine($"{CarName} Rough engine, new speed: {MaxSpeed}");
+            
+        }
+
+        public void TimeOut(int Time)
+        {
+            decimal temp = CurrentSpeed;
+
+            CurrentSpeed = 0;
+            Task.Delay(Time / warp);
+            CurrentSpeed = temp;
+
+        }
+
+
+
+        public void Drive()
+        {
+            int timeLapsed = 0;
+            int totalTime = 0;
+
+            
+            
+            
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.CursorTop = 0;
+
+                // km/h
+                if(CurrentSpeed != 0)
+                {
+
+                    CurrentSpeed = MaxSpeed;
+                }
+                decimal distancePerSecond = CurrentSpeed / 3600;
+                decimal displayDistance = Math.Round(TraveledDistance, 5);
+
+                Console.WriteLine($"Speed:{CurrentSpeed} Distance: {displayDistance}km");
+                Thread.Sleep(1000 / warp);
+
+
+                //Console.BackgroundColor = Color;
+                //30sec?
+                if(timeLapsed == 30)
+                {
+                    Console.CursorTop = 5;
+
+                    timeLapsed = 0;
+                    Console.WriteLine("30 sek");
+                    Console.WriteLine("Distance:" + displayDistance + " km? " + CurrentSpeed);
+                    //Console.ReadLine();
+
+                    //Event
+                    RandomEvent();
+
+
+                }
+
+                timeLapsed++;
+                totalTime++;
+
+                TraveledDistance += distancePerSecond;
+
+
+
+
+
+            }
+
+
+        }
+
+        public void Start()
+        {
+            CurrentSpeed = MaxSpeed;
+            Console.WriteLine($"{CarName} started");
+            Drive();
+        }
+
+
+        public async Task RandomEvent()
+        {
+
+            Random rand = new Random();
+            int randomNumber = rand.Next(49);
+
+            if (randomNumber == 0)
+            {
+                //Console.WriteLine("Tanka"); // 2%
+
+                await RunOutOfFuel();
+            }
+            if (1 <= randomNumber && randomNumber <= 2)
+            {
+                //Console.WriteLine("Nya däck"); // 4%
+                await FlatTire();
+
+            }
+            if (3 <= randomNumber && randomNumber <= 7)
+            {
+
+                //Console.WriteLine("Fågel!"); // 10%
+                await BirdStrike();
+            }
+            if (8 <= randomNumber && randomNumber <= 17)
+            {
+                //Console.WriteLine("Motorproblem"); //20%
+                await RoughEngine();
+            }
+
+
+        }
+    }
+}
---
+++ CarRace/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace CarRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RunRace();
+
+            
+
+        }
+
+
+
+        static void RunRace()
+        {
+
+            Car car1 = new Car("Volvo");
+
+            car1.Start();
+
+            //Random();
+
+
+        }
+
+
+
+
+
+        public static void Random()
+        {
+            double turns = 1e6;
+
+            double a = 0; //2%
+            double b = 0; //4%
+            double c = 0; //10%
+            double d = 0; //20%
+
+
+    
+
+
+            for (int i = 0; i < turns; i++)
+            {
+
+
+                Random rand = new Random();
+                int randomNumber = rand.Next(49);
+
+                if (randomNumber == 0)
+                {
+                    //Console.WriteLine("Tanka"); // 2%
+                    a++;
+                }
+                if (1 <= randomNumber && randomNumber <= 2)
+                {
+                    //Console.WriteLine("Nya däck"); // 4%
+                    b++;
+
+                }
+                if (3 <= randomNumber && randomNumber <= 7)
+                {
+
+                    //Console.WriteLine("Fågel!"); // 10%
+                    c++;
+                }
+                if (8 <= randomNumber && randomNumber <= 17)
+                {
+                    //Console.WriteLine("Motorproblem"); //20%
+                    d++;
+                }
+            }
+
+            Console.WriteLine($"{(a / turns) * 100 }: 2%?");
+            Console.WriteLine($"{(b / turns) * 100 }: 4%?");
+            Console.WriteLine($"{(c / turns) * 100 }: 10%?");
+            Console.WriteLine($"{(d / turns) * 100 }: 20%?");
+
+            /*
+             0 : + 30 sek tanka
+             1-2: + 20 sek nya däck
+             3-7: + 10 sek fågel
+             8-18: -1km/h 
+             
+             */
+
+        }
+    }
+}
